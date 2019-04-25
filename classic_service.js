058cc806a9f3b06b17c386cba565e0b5d46f249b
GrimThor3 <32375221+GrimThor3@users.noreply.github.com>
2019-04-25 12:03:01 -0700

    Update classic_service.js
    v1.6: Updates to file paths when accessing text fils

--- classic_service.js
+++ classic_service.js
@@ -19,14 +19,14 @@ app.set('port', process.env.PORT || 3000);
 app.get('/', function (req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
 
-	let mode = req.mode;
+	let mode = req.query.mode;
 	console.log("Mode: " + mode);
 	if (mode === "signup") {
 		let name = req.query.name;
 		let email = req.query.email;
 		let password = req.query.password;
 
-		let dir = 'users/' + escape(email);
+		let dir = 'public/users/' + escape(email);
 
 		if (!fs.existsSync(dir)) {
 			fs.mkdir(dir, { recursive: true }, function(err) {
@@ -34,16 +34,16 @@ app.get('/', function (req, res) {
 	 				throw err;
 	 			}
 	 		});
-	 		let dir2 = 'users/' + escape(email) + '/settings.txt';
+	 		let dir2 = 'public/users/' + escape(email) + '/settings.txt';
 	 		let contents = "Name: " + escape(name) + "\nPassword: " + escape(password) + "\nImage Source: img/noimg.png";
 	 		fs.writeFileSync(dir2, contents);
-	 		let dir3 = 'users/' + escape(email);
+	 		let dir3 = 'public/users/' + escape(email);
 	 		fs.mkdir(dir3, { recursive: true }, function (err) {
 	 			if (err) {
 	 				throw err;
 	 			}
 	 		});
-	 		let dir4 = 'users/' + escape(email) + '/status.txt';
+	 		let dir4 = 'public/users/' + escape(email) + '/status.txt';
 	 		fs.writeFileSync(dir4, "");
 		} else {
 			res.send("Incorrect creditentials");
@@ -54,14 +54,14 @@ app.get('/', function (req, res) {
 
 		console.log(email);
 
-		let settingPath = "users/" + escape(email) + "/settings.txt";
+		let settingPath = "public/users/" + escape(email) + "/settings.txt";
 		if (fs.existsSync(settingPath)) {
 			let file = fs.readFileSync(settingPath, 'utf8');
 			let fileContents = file.split('\n');
 			if (fileContents[0].split(": ")[1] == password) {
 				let data = {};
 				data["image"] = fileContents[1].split(": ")[1];
-				let statusPath = 'users/' + escape(email) + '/status.txt';
+				let statusPath = 'public/users/' + escape(email) + '/status.txt';
 				data["status"] = fs.readFile(statusPath, function(err) {
 					if (err) {
 						throw err;
@@ -77,8 +77,8 @@ app.get('/', function (req, res) {
 		console.log("hit");
 		let email = req.query.email;
 
-		let setDir = 'users/' + escape(email) + '/settings.txt';
-		let statusDir = 'users/' + escape(email) + '/status.txt';
+		let setDir = 'public/users/' + escape(email) + '/settings.txt';
+		let statusDir = 'public/users/' + escape(email) + '/status.txt';
 
 		let settingsFile = fs.readFileSync(setDir, 'utf8');
 		let statusFile = fs.readFileSync(statusDir, 'utf8');
