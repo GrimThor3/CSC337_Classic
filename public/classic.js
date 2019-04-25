c0966e0ebee5b13efaa55a27b8fdcecad4de4fe8
GrimThor3 <32375221+GrimThor3@users.noreply.github.com>
2019-04-24 18:25:48 -0700

    Add files via upload
    v1.0 of classic

---
+++ public/classic.js
@@ -0,0 +1,156 @@
+"use strict";
+
+window.onload = (function() {
+	document.getElementById("login_button").onclick = login;
+	document.getElementById("switch_signup").onclick = switchToSignUp;
+	document.getElementById("signup_button").onclick = signup;
+	document.getElementById("switch_login").onclick = switchToLogIn;
+	document.getElementById("searchButton").onclick = (function() {
+		let email = document.getElementById("search").value;
+		if (email != "" && email != "Search for friends via email") {
+			console.log("searching for " + email);
+			loadUser(email);
+		}
+	});
+	document.getElementById("Log off").onclick = switchToSignUp;
+
+	function login() {
+		let email = document.getElementById("li_email").value;
+		let password = document.getElementById("li_password").value;
+
+		let port = process.env.PORT || 3000;
+		let url = "";
+		if (port === 3000) {
+			url = "http://localhost:3000";
+		} else {
+			url = "https://app.herokuapp.com/";
+		}
+
+		url = url + "?mode=login&email=" + email + "&password=" + password;
+		
+		fetch(url)
+			.then(checkStatus)
+			.then(function(responseText) {
+
+			})
+			.catch(function(error) {
+				console.log(error);
+				return;
+			});
+
+		loadUser(email);
+	}
+
+	function switchToSignUp() {
+		let loginPage = document.getElementById("login");
+		let signupPage = document.getElementById("signup");
+		let page = document.getElementById("hasLoggedIn");
+
+		loginPage.style.display = 'None';
+		signupPage.style.display = 'Block';
+		page.style.dispaly = 'None';
+	}
+
+	function signup() {
+		let fname = document.getElementById("su_fname").value;
+		let lname = document.getElementById("su_lname").value;
+		let email = document.getElementById("su_email").value;
+		let password = document.getElementById("su_password").value;
+
+		let port = process.env.PORT || 3000;
+		let url = "";
+		if (port === 3000) {
+			url = "http://localhost:3000";
+		} else {
+			url = "https://app.herokuapp.com/";
+		}
+
+		url = url + "?mode=signup&name=" + fname + "_" + 
+			lname + "&email=" + email + "&password=" + password;
+		
+		fetch(url)
+			.then(checkStatus)
+			.then(function(responseText) {
+
+			})
+			.catch(function(error) {
+				console.log(error);
+				return;
+			});
+
+		loadUser(email);
+	}
+
+	function switchToLogIn() {
+		let loginPage = document.getElementById("login");
+		let signupPage = document.getElementById("signup");
+		let page = document.getElementById("hasLoggedIn");
+
+		loginPage.style.display = 'Block';
+		signupPage.style.display = 'None';
+		page.style.dispaly = 'None';
+	}
+
+	function loadUser(email) {
+		let loginPage = document.getElementById("login");
+		let signupPage = document.getElementById("signup");
+		let page = document.getElementById("hasLoggedIn");
+
+		loginPage.style.display = 'None';
+		signupPage.style.display = 'None';
+		page.style.display = 'Block';
+
+		let port = process.env.PORT || 3000;
+		let url = "";
+		if (port === 3000) {
+			url = "http://localhost:3000";
+		} else {
+			url = "https://app.herokuapp.com/";
+		}
+
+		url = url + "http://localhost:3000?mode=getuser&email=" + email;
+		console.log(url);
+		fetch(url)
+			.then(checkStatus)
+			.then(function(responseText) {
+				if (responseText == "Incorrect creditentials") {
+					document.getElementById("other").innerHTML = "User not found";
+				}
+				let json = JSON.parse(responseText);
+
+				let name = json['name'].replace('_', ' ');
+				console.log("Name: " + name);
+				document.getElementById("Name").innerHTML = name;
+
+				console.log("Img Src: " + json['imgsrc']);
+				document.getElementById("profilePic").src = json['imgsrc'];
+
+				let status = json['status'];
+				console.log("Status: " + status);
+
+				if (status === '') {
+					status = '~ No status has been posted ~';
+				}
+
+				document.getElementById("status").innerHTML = status;
+			})
+			.catch(function(error) {
+				console.log(error);
+				return;
+
+			})
+	}
+
+	function checkStatus(response) {
+		if (response.status >= 200 && response.status < 300) {
+			return response.text();
+		} else if (response.status === 404) {
+			return Promise.reject(new Error("Sorry, we couldn't find that page"));
+		} else if (response.status === 410) {
+			return Promise.reject(new Error(response.status + ": " + "There was no data to display"))
+		} else {
+			console.log(response.text());
+			return Promose.reject(new Error(reponse.status + ": " + reponse.statusText))
+		}
+	}
+});
---
+++ public/classic_service.js
@@ -0,0 +1,100 @@
+/*
+* File Name: mymdb.js
+* Author: Rafael Jeffery
+* Purpose: Server-side code
+*/
+"use strict";
+
+const express = require("express");
+const app = express();
+let session = require("client-sessions");
+let fs = require('fs');
+let bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+console.log('Web service started');
+
+
+app.get('/', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+
+	let mode = req.query.mode;
+	console.log("Mode: " + mode);
+	if (mode === "signup") {
+		let name = req.query.name;
+		let email = req.query.email;
+		let password = req.query.password;
+
+		let dir = 'users/' + escape(email);
+
+		if (!fs.existsSync(dir)) {
+			fs.mkdir(dir, { recursive: true }, function(err) {
+	 			if (err) {
+	 				throw err;
+	 			}
+	 		});
+	 		let dir2 = 'users/' + escape(email) + '/settings.txt';
+	 		let contents = "Name: " + escape(name) + "\nPassword: " + escape(password) + "\nImage Source: img/noimg.png";
+	 		fs.writeFileSync(dir2, contents);
+	 		let dir3 = 'users/' + escape(email);
+	 		fs.mkdir(dir3, { recursive: true }, function (err) {
+	 			if (err) {
+	 				throw err;
+	 			}
+	 		});
+	 		let dir4 = 'users/' + escape(email) + '/status.txt';
+	 		fs.writeFileSync(dir4, "");
+		} else {
+			res.send("Incorrect creditentials");
+		}
+	} else if (mode === "login") {
+		let email = req.query.email;
+		let password = req.query.password;
+
+		console.log(email);
+
+		let settingPath = "users/" + escape(email) + "/settings.txt";
+		if (fs.existsSync(settingPath)) {
+			let file = fs.readFileSync(settingPath, 'utf8');
+			let fileContents = file.split('\n');
+			if (fileContents[0].split(": ")[1] == password) {
+				let data = {};
+				data["image"] = fileContents[1].split(": ")[1];
+				let statusPath = 'users/' + escape(email) + '/status.txt';
+				data["status"] = fs.readFile(statusPath, function(err) {
+					if (err) {
+						throw err;
+					}
+				});
+			} else {
+				res.send("Incorrect creditentials");
+			}
+		} else {
+			res.send("Incorrect creditentials");
+		}
+	} else if (mode === "getuser") {
+		console.log("hit");
+		let email = req.query.email;
+
+		let setDir = 'users/' + escape(email) + '/settings.txt';
+		let statusDir = 'users/' + escape(email) + '/status.txt';
+
+		let settingsFile = fs.readFileSync(setDir, 'utf8');
+		let statusFile = fs.readFileSync(statusDir, 'utf8');
+
+		let data = {};
+
+		let setFileContents = settingsFile.split('\n');
+		data['name'] = setFileContents[0].split(': ')[1];
+		data['imgsrc'] = setFileContents[2].split(': ')[1];
+		data['status'] = statusFile;
+
+		console.log(JSON.stringify(data));
+
+		res.send(JSON.stringify(data));
+	} else if (mode === "changeimg") {
+		let email = req.query.email;
+	}
+});
+
+app.listen(process.env.PORT || 3000);
