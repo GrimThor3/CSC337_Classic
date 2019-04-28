def4ec710b79156ccfc6f63e6719d661d2b0e9e4
GrimThor3 <32375221+GrimThor3@users.noreply.github.com>
2019-04-27 21:49:16 -0700

    Add files via upload

--- classic_service.js
+++ classic_service.js
@@ -1,5 +1,5 @@
 /*
-* File Name: mymdb.js
+* File Name: classic_service.js
 * Author: Rafael Jeffery
 * Purpose: Server-side code
 */
@@ -7,26 +7,29 @@
 
 const express = require("express");
 const app = express();
-let session = require("client-sessions");
 let fs = require('fs');
-let bodyParser = require('body-parser');
+let process = require('process');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
 
 app.use(express.static('public'));
 console.log('Web service started');
 
+// Sets port to either heroku port or 3000 (local host)
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
-
-	let mode = req.query.mode;
+	console.log(req);
+	let mode = req.mode;
 	console.log("Mode: " + mode);
 	if (mode === "signup") {
 		let name = req.query.name;
 		let email = req.query.email;
 		let password = req.query.password;
 
-		let dir = 'public/users/' + escape(email);
+		let dir = 'users/' + escape(email);
 
 		if (!fs.existsSync(dir)) {
 			fs.mkdir(dir, { recursive: true }, function(err) {
@@ -34,16 +37,16 @@ app.get('/', function (req, res) {
 	 				throw err;
 	 			}
 	 		});
-	 		let dir2 = 'public/users/' + escape(email) + '/settings.txt';
+	 		let dir2 = 'users/' + escape(email) + '/settings.txt';
 	 		let contents = "Name: " + escape(name) + "\nPassword: " + escape(password) + "\nImage Source: img/noimg.png";
 	 		fs.writeFileSync(dir2, contents);
-	 		let dir3 = 'public/users/' + escape(email);
+	 		let dir3 = 'users/' + escape(email);
 	 		fs.mkdir(dir3, { recursive: true }, function (err) {
 	 			if (err) {
 	 				throw err;
 	 			}
 	 		});
-	 		let dir4 = 'public/users/' + escape(email) + '/status.txt';
+	 		let dir4 = 'users/' + escape(email) + '/status.txt';
 	 		fs.writeFileSync(dir4, "");
 		} else {
 			res.send("Incorrect creditentials");
@@ -54,14 +57,14 @@ app.get('/', function (req, res) {
 
 		console.log(email);
 
-		let settingPath = "public/users/" + escape(email) + "/settings.txt";
+		let settingPath = "users/" + escape(email) + "/settings.txt";
 		if (fs.existsSync(settingPath)) {
 			let file = fs.readFileSync(settingPath, 'utf8');
 			let fileContents = file.split('\n');
 			if (fileContents[0].split(": ")[1] == password) {
 				let data = {};
 				data["image"] = fileContents[1].split(": ")[1];
-				let statusPath = 'public/users/' + escape(email) + '/status.txt';
+				let statusPath = 'users/' + escape(email) + '/status.txt';
 				data["status"] = fs.readFile(statusPath, function(err) {
 					if (err) {
 						throw err;
@@ -77,8 +80,8 @@ app.get('/', function (req, res) {
 		console.log("hit");
 		let email = req.query.email;
 
-		let setDir = 'public/users/' + escape(email) + '/settings.txt';
-		let statusDir = 'public/users/' + escape(email) + '/status.txt';
+		let setDir = 'users/' + escape(email) + '/settings.txt';
+		let statusDir = 'users/' + escape(email) + '/status.txt';
 
 		let settingsFile = fs.readFileSync(setDir, 'utf8');
 		let statusFile = fs.readFileSync(statusDir, 'utf8');
